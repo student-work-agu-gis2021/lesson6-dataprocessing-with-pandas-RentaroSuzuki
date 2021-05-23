59ff58e080ab9a20737197f5099fd5f6a606adcc
RentaroSuzuki <>
2021-05-23 16:55:30 +0000

    Modified Ex6_1 L20~23
    I add now variable(data) to read the file.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -17,10 +17,10 @@ import numpy as np
 data = None
 
 # YOUR CODE HERE 1
-fp="data.txt"
-data=pd.read_csv(fp,delim_whitespace=True,header=0,skiprows=1,na_values=-9999)
-print(data)
-print(data.columns)
+fp="data/1091402.txt"
+data=pd.read_csv(fp,delim_whitespace=True,header=0,skiprows=[1],na_values=[-9999])
+print(data.hrad())
+print(data.tall())
 
 # ### Part 2 
 # 
