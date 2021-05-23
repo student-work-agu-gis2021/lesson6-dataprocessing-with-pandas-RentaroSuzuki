ad834fc72f821afe7fd89a2cea8e24fb169615ad
RentaroSuzuki <>
2021-05-23 17:01:14 +0000

    Modified Ex6_1 L50
    I add a new variable(tmin_nodata_count) to calculate how many no-data (NaN) values there are for the TMIN column

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -47,11 +47,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
-for idx, row in data.iterrows():
-  if np.isnan(data['TMIN']):
-    tmin_nodata_count += 1
-  break
-print(tmin_nodata_count)
+tmin_nodata_count=data.iloc[:,8].usnull().sum()
 
 
 #CAUTION!!! DON'T EDIT THIS PART START
